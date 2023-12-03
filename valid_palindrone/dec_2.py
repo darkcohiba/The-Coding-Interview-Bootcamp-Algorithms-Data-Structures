9e1e24023cb1f42333e0019c1bda4700083ad658
Sam Waters <samuel.waters@gmail.com>
2023-12-02 23:00:37 -0700

    chore: started valid palindrone

--- valid_palindrone/dec_2.py
+++ valid_palindrone/dec_2.py
@@ -1,5 +1,19 @@
 def isPalindrome(s: str) -> bool:
     return (s)
 
+# steps
+# Normalize the String:
+
+# Convert the entire string to lowercase.
+# Remove all characters that are not letters or numbers.
+# Check for Palindrome:
+
+# Compare characters from the start and the end of the string, moving towards the center.
+# If all corresponding characters match, it's a palindrome.
+# If any do not match, it's not a palindrome.
+# Return the Result:
+
+# Your function should return true if the string is a palindrome, and false otherwise.
+
 
 print(isPalindrome("a man i nam a"))
