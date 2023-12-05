5ba59ac59ca7bc4d35c6453a72eb08b8ea4d6f6e
Sam Waters <samuel.waters@gmail.com>
2023-12-04 21:18:04 -0700

    anagram finished

--- valid_anagram/dec_4.py
+++ valid_anagram/dec_4.py
@@ -1,6 +1,24 @@
 def isAnagram(s, t) -> bool:
-    letter_map = {}
-    for index, char in enumerate(s):
-        letter_map[char] = index
-        
+    if len(s) != len(t):
+        return False
 
+    # Create a count map for each character in s
+    letter_count = {}
+    for char in s:
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+
+    # Decrement the count for each character in t
+    for char in t:
+        if char not in letter_count:
+            return False
+
+        letter_count[char] -= 1
+        if letter_count[char] < 0:
+            return False
+    return True
+
+print(isAnagram('anagram', 'nagaram'))  # Should return True
+print(isAnagram('rat', 'car'))  # Should return False
