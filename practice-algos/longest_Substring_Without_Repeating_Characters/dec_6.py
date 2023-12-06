7a9985ebcb8d8bb53ee4bec8b28dbaee52490a4f
Sam Waters <samuel.waters@gmail.com>
2023-12-06 09:12:50 -0700

    added Longest Substring Without Repeating Characters

---
+++ practice-algos/longest_Substring_Without_Repeating_Characters/dec_6.py
@@ -0,0 +1,7 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    print(s)
+
+
+print(lengthOfLongestSubstring("abcabcbb"))
+print(lengthOfLongestSubstring("bbbbb"))
+print(lengthOfLongestSubstring("pwwkew"))
