be91708c00a6f9426c4026519afc1d244a4bc503
Sam Waters <samuel.waters@gmail.com>
2023-12-15 08:37:57 -0700

    hackerrank sock sales

---
+++ practice-algos/sock_merchant/dec_15.py
@@ -0,0 +1,22 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    socks = {}
+    
+
+
+sockMerchant(7, [1, 2, 1, 2, 3, 1, 2])
