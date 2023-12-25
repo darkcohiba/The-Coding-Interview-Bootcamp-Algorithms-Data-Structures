1aa9ec4fe10fd248251e03c232303c6eec7ad229
Sam Waters <samuel.waters@gmail.com>
2023-12-25 09:23:17 -0700

    christmas practice

---
+++ practice-algos/best_time_to_buy_and_sell_stocks/dec_25.py
@@ -0,0 +1,8 @@
+def maxProfit(stocks):
+    max_profit = 0
+    min_price = stocks[0]
+    if min_price
+
+print(maxProfit([7,1,5,3,6,4]))
+print(maxProfit([2,1,5,2,6,4]))
+print(maxProfit([4,1,3,3,6,4]))
