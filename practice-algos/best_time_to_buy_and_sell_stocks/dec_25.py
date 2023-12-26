0fc01232372e59645e1453a780089a1b5960888d
Sam Waters <samuel.waters@gmail.com>
2023-12-26 13:47:44 -0700

    christmas practice

--- practice-algos/best_time_to_buy_and_sell_stocks/dec_25.py
+++ practice-algos/best_time_to_buy_and_sell_stocks/dec_25.py
@@ -4,8 +4,11 @@ def maxProfit(stocks):
     for stock in stocks:
         if stock < min_price:
             min_price = stock
-        elif 
+        # first number is 7, the stock will not be greater
+        elif stock - min_price > max_profit:
+            max_profit = stock - min_price
+    return max_profit
 
 print(maxProfit([7,1,5,3,6,4]))
 print(maxProfit([2,1,5,2,6,4]))
-print(maxProfit([4,1,3,3,6,4]))
+print(maxProfit([4,1,3,3,3,4]))
